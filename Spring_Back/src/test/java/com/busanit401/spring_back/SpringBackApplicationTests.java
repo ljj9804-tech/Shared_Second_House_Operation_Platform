4f2e8d67b43b71e2f09a1bf11ba3b29817356bb0
leejinju <ljj9804@gmail.com>
2026-06-04 17:26:49 +0900

    feat: 초기 작업파일 생성(test파일 이후에 삭제 예정입니다)

--- Spring_Back/src/test/java/com/busanit401/spring_back/SpringBackApplicationTests.java
+++ Spring_Back/src/test/java/com/busanit401/spring_back/SpringBackApplicationTests.java
@@ -1,7 +1,7 @@
 //package com.busanit401.spring_back;
 //
 //import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.memo.context.SpringBootTest;
 //
 //@SpringBootTest
 //class SpringBackApplicationTests {
