83a5f1807ddad09131b4440f7a06b9a8bb807721
leejaeuk0718 <jaeuk2210@naver.com>
2026-06-05 16:26:09 +0900

    유저에 필요한 엔티티 및 이넘 작업 중

--- Spring_Back/src/main/java/com/busanit401/spring_back/domain/PaymentRefund.java
+++ Spring_Back/src/main/java/com/busanit401/spring_back/domain/PaymentRefund.java
@@ -30,7 +30,7 @@ public class PaymentRefund {
 
     // 해당 월 환불 금액
     @Column(name = "refund_amount", nullable = false)
-    private int refundAmount;
+    private Long refundAmount;
 
     // 토스페이먼츠 환불 처리 후 받는 취소 키
     @Column(name = "cancel_key")
--- Spring_Back/src/main/java/com/busanit401/spring_back/enums/SubscriptionStatus.java
+++ Spring_Back/src/main/java/com/busanit401/spring_back/enums/SubscriptionStatus.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SubscriptionStatus {
-
-    PENDING("승인 대기"),
     ACTIVE("ACTIVE_USER"),     // 구독 중
     EXPIRED("EXPIRED_USER"),    // 만료
     CANCELLED("CANCELLED_USER"); // 취소
