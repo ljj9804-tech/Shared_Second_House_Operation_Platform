0059380e5d04b30b6f0257f817e276e9cc051d42
정성규 <jsg213213@naver.com>
2026-06-08 15:11:19 +0845

    260608

---
+++ Spring_Back/src/main/java/com/busanit401/spring_back/Repository/DeliveryOrderRepository.java
@@ -0,0 +1,12 @@
+package com.busanit401.spring_back.Repository;
+
+import com.busanit401.spring_back.domain.DeliveryOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface DeliveryOrderRepository extends JpaRepository<DeliveryOrder, Long> {
+    // 유저별 최신 주문 내역 조회 레코드 (추후 내비게이션이나 마이페이지 연동용)
+    List<DeliveryOrder> findByUserIdOrderByCreatedAtDesc(Long userId);
+}
---
+++ Spring_Back/src/test/java/com/busanit401/spring_back/SpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.busanit401.spring_back;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.memo.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBackApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
