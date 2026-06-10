6d9433c1902fd451450592dba8d9c81419fad3d6
leejinju <ljj9804@gmail.com>
2026-06-10 17:48:53 +0900

    feat: 웹소켓 서버 연동, 게스트 채팅방 UI 구현 chore: pubspec.yaml 라이브러리 추가(http: ^1.2.0)

--- flutter_front/lib/domain/service/guest_chat_service.dart
+++ flutter_front/lib/domain/service/guest_chat_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:stomp_dart_client/stomp_dart_client.dart';
+import 'package:http/http.dart' as http;
 
 class GuestChatService {
   StompClient? _stompClient;
@@ -7,9 +8,38 @@ class GuestChatService {
 
   // 백엔드 컴퓨터의 IP 주소 (안드로이드 에뮬레이터 기준 10.0.2.2)
   final String _wsUrl = 'ws://10.0.2.2:8080/ws-guest-chat';
+  // 1) 에뮬레이터로 테스트할 때
+  final String _baseUrl = 'http://10.0.2.2:8080/api/guest/chat';
+  // 2) 실물 스마트폰으로 테스트할 때 (💡 본인 컴퓨터의 실제 IP 주소로 수정하세요!)
+  // static const String _ip = '222.234.36.85:8080';
 
   bool get isConnected => _isConnected;
 
+  // 채팅방의 과거 메시지 데이터 가져오기 (HTTP GET)==============================================================================================
+  Future<List<Map<String, dynamic>>> getChatHistory(int chatRoomId) async {
+    final url = Uri.parse('$_baseUrl/history/$chatRoomId');
+
+    try {
+      print('🌐 [플러터 HTTP] 과거 내역 요청 시작 -> $url');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        // ✨ 한글 깨짐 방지를 위해 UTF-8로 먼저 디코딩 후 JSON 파싱합니다.
+        final List<dynamic> decodedData = jsonDecode(utf8.decode(response.bodyBytes));
+
+        // List<dynamic> 구조를 데이터 다루기 편하게 List<Map<String, dynamic>>으로 안전하게 성형하여 반환합니다.
+        return decodedData.map((item) => item as Map<String, dynamic>).toList();
+      } else {
+        print('❌ [플러터 HTTP] 서버 에러 발생 (코드: ${response.statusCode})');
+        return [];
+      }
+    } catch (e) {
+      print('❌ [플러터 HTTP] 통신 중 네트워크 예외 에러 발생: $e');
+      return [];
+    }
+  }
+
+  // 웹소켓 서버 연결 작업 로직==============================================================================================
   /// [1] 웹소켓 서버 초기화 및 연결 시작
   void connectWebSocket({
     required int chatRoomId,
