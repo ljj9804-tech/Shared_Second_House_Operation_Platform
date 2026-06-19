73c67c6bec45696236188ed424a81755e389e58b
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-06-19 14:45:58 +0900

    feat: [스프링] 숙소관련 이미지 추가 + BaseTimeEntity 활성화

--- Spring_Back/src/main/java/com/busanit401/spring_back/SpringBackApplication.java
+++ Spring_Back/src/main/java/com/busanit401/spring_back/SpringBackApplication.java
@@ -2,9 +2,11 @@ package com.busanit401.spring_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaAuditing // JPA Auditing 활성화 - BaseTimeEntity의 createdDate, modifiedDate 자동 입력에 필요 (필수!)
 @SpringBootApplication
 public class SpringBackApplication {
 
