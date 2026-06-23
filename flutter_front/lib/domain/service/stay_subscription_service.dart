cf10d1391233d122166ef67f08bd010f6913e044
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-06-23 09:41:05 +0900

    feat: [플러터] 기능 수정 + 오류 수정 + 주석처리

--- flutter_front/lib/domain/service/stay_subscription_service.dart
+++ flutter_front/lib/domain/service/stay_subscription_service.dart
@@ -1,3 +1,22 @@
+/*
+ * ==================================================================================
+ * [파일 정보]
+ * 위치  : lib/domain/service/stay_subscription_service.dart
+ * 역할  : 구독 신청 / 조회 API 통신 레이어
+ * 사용처 : StayAccommodationController, StaySubscriptionApplyScreen
+ * ----------------------------------------------------------------------------------
+ * [연관 파일]
+ * - stay_accommodation_controller.dart   : getMySubscriptions 호출
+ * - stay_subscription_apply_screen.dart  : applySubscription 호출
+ * - Spring: SubscriptionsController.java : /subscriptions, /waiting
+ * ----------------------------------------------------------------------------------
+ * [메서드 목록]
+ * - applySubscription()      : 구독 신청 POST /waiting/apply/{leaderId}
+ * - getMySubscriptions(id)   : 내 구독 목록 GET /subscriptions/my/{userId}
+ * - getMyInvitations(id)     : 내 초대 목록 GET /waiting/my/{userId}
+ * ==================================================================================
+ */
+
 import 'package:dio/dio.dart';
 import 'package:flutter_front/config/app_config.dart';
 
